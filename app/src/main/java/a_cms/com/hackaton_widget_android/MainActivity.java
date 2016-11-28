8c030cbae81e6b7142e0197343158b4e2a031326
BayrekMokni <mohamed-bayrek.mokni@viseo.com>
2016-11-28 16:56:48 +0100

    Initialize android widget project

---
+++ app/src/main/java/a_cms/com/hackaton_widget_android/MainActivity.java
@@ -0,0 +1,13 @@
+package a_cms.com.hackaton_widget_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
